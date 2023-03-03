8249630cdadff2b3bf15a37e88671be2eb8bbd34
Gubera24 <114172003+Gubera24@users.noreply.github.com>
2023-03-03 08:17:46 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -37,13 +37,14 @@ class mr(object):
     ABOUT_TXT = """
 ╭───────────⍟
 ├🤖 The Name is : {}
-├👑 ADMIN-TG : <a href=https://t.me/LazyDeveloper>LazyDeveloper</a> 
-├👑 Instagram : <a href=https://www.instagram.com/LazyDeveloperrr>C-Insta</a> 
+├👑 ADMIN : <a href=https://t.me/Tamilanda29>Owner</a> 
+├❤️‍🔥 Ceo : <a href=https://t.me/Tamilanda35>Co-Owner</a> 
 ├📕 LIBRARY : <a href=https://github.com/pyrogram>Pyrogram</a>
 ├✏️ LANGUAGE : <a href=https://www.python.org>Python 3</a>
 ├💾 DATABASE : <a href=https://cloud.mongodb.com>MongoDB</a>
 ├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁1 : <a href=https://apps.koyeb.com>Koyeb</a>
-├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁2 : <a href=https://dashboard.heroku.com>Heroku</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁2 : <a href=https://Render.com>Render</a>
+├❣️ Source Code : <a href=https://github.com/Gubera24/Gang-Horror>Repo</a>
 ├📊 BUILD-STATUS : Python v3.6.8               
 ╰───────────────⍟
                                 """
@@ -65,18 +66,18 @@ Example:- /set_caption 📕 File Name: {filename}
 
 ✏️ <b><u>HOW TO RENAME A FILE</u></b>
 •> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
-ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://www.instagram.com/LazyDeveloperrr>Direct Message</a>
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/Tamilanda29>Direct Message</a>
 """
 
 #⚠️ don't remove our credits 🙏😢😢
     DEV_TXT = """
 <b><u>Special Thanks & Developers</b></u> 
 
-» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://www.youtube.com/channel/UCY-iDra0x2hdd9PdHKcZkRw>Watch Tutorial</a>
+» ❣️Source Code : <a href=https://github.com/Gubera24/Gang-Horror>Repo</a>
 
-• 😈 <a href=https://github.com/LazyDeveloperr>LazyDeveloper Github</a>
-• 😈 <a href=https://t.me/LazyDeveloper>LazyDeveloper Tg Channel</a>
-• 😈 <a href=https://t.me/mRiderDM>Direct-Msg-TG</a>
-• 😈 <a href=https://t.me/real_MoviesAdda1>Join Movies Adda</a>
-• 😈 <a href=https://t.me/GitHub_noob>Thanks- Github Noob</a>
+• 😈 <a href=https://t.me/Tamilanda29>Owner</a>
+• 😈 <a href=https://t.me/Tamilanda35>Co-Owner</a>
+• 😈 <a href=https://github.com/Gubera24>Github</a>
+• 😈 <a href=https://t.me/+-dxpAX6SVhliMGVl>Horror Movie's Tamil</a>
+• 😈 <a href=https://t.me/thesoulesssoul>valuable Patner</a>
 """
