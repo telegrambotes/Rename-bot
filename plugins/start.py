dd3765417d55096291bcb90585710e1e4d3ecd0b
Gubera24 <114172003+Gubera24@users.noreply.github.com>
2023-03-11 17:36:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -78,14 +78,14 @@ async def rename_start(client, message):
     fileid = file.file_id
     try:
         text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
-        buttons = [[ InlineKeyboardButton("📝 START HACKING  📝", callback_data="rename") ],
+        buttons = [[ InlineKeyboardButton("👻 START HACKING 👻", callback_data="rename") ],
                    [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
         await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
         await sleep(FLOOD)
     except FloodWait as e:
         await sleep(e.value)
         text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
-        buttons = [[ InlineKeyboardButton("📝 START HACKING 📝", callback_data="rename") ],
+        buttons = [[ InlineKeyboardButton("👻 START HACKING 👻", callback_data="rename") ],
                    [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
         await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
     except:
